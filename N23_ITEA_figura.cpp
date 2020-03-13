83754b922cb800b0a391332a84150dd5a02b895c
Oleksiy <voriansevrat@gmail.com>
2020-03-13 22:07:08 +0200

    2-d commit

---
+++ N23_ITEA_figura.cpp
@@ -0,0 +1,16 @@
+﻿#include <iostream>
+#include <conio.h>
+#include "figura.h"
+
+int main(int argc, char**argv) {
+  Figura f;
+  std::cin >> f;
+
+  Circle c(f);
+  std::cout << c.Area() << std::endl;
+
+  Rectangle g(f);
+  std::cout << g.Area() << std::endl;
+
+  return 0;
+}
